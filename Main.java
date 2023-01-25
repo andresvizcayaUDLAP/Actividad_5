ece3e25efd8d36a4b5e2bd2908347ccefd042a98
andresvizcayaUDLAP <122307881+andresvizcayaUDLAP@users.noreply.github.com>
2023-01-25 12:07:10 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		String opcion;
-		String opcion1;
+		char opcion1;
 		Scanner scan = new Scanner(System.in);
 		boolean validacion;
 		
@@ -26,15 +26,15 @@ public class Main {
 				System.out.println("c) Area");
 				
 				System.out.println("Indica la letra de la opción que deseas calcular: ");
-				opcion1 = scan.nextLine();
+				opcion1 = scan.next().charAt(0);
 				
 				Cuadrado cuadrado1 = new Cuadrado(lado);
 				System.out.println("Cuadrado");
 				
 				switch(opcion1) {
-				case "a": System.out.println("Diagonal: " + cuadrado1.diagonal()); break;
-				case "b": System.out.println("Perímetro: " + cuadrado1.perimetro()); break;
-				case "c": System.out.println("Área: " + cuadrado1.area()); break;
+				case 'a': System.out.println("Diagonal: " + cuadrado1.diagonal()); break;
+				case 'b': System.out.println("Perímetro: " + cuadrado1.perimetro()); break;
+				case 'c': System.out.println("Área: " + cuadrado1.area()); break;
 				default: System.out.println("No es válido"); break;
 				}
 	
@@ -49,30 +49,29 @@ public class Main {
 				System.out.println("b) Área");
 				
 				System.out.println("Indica la letra de la opción que deseas calcular: ");
-				opcion1 = scan.nextLine();
+				opcion1 = scan.next().charAt(0);
 				System.out.println(radio);
 			
 				Circulo circulo1 = new Circulo(radio);
 				System.out.println("Círculo");
 				
 				switch(opcion1) {
-					case "a": System.out.println("Circunferencia: " + circulo1.circunferencia()); break;
-					case "b": System.out.println("Área: " + circulo1.area()); break;
+					case 'a': System.out.println("Circunferencia: " + circulo1.circunferencia()); break;
+					case 'b': System.out.println("Área: " + circulo1.area()); break;
 					default: System.out.println("No es válido");  break;
 				}
 				
 			} else System.out.println("Elige una opción correcta");
 			
-			if(opcion.equalsIgnoreCase("cuadrado")) {
-				validacion = true;
-			} else if(opcion.equalsIgnoreCase("circulo")) {
+			if(opcion.equalsIgnoreCase("cuadrado")||opcion.equalsIgnoreCase("circulo")) {
 				validacion = true;
 			} else {
 				validacion = false;
 			}
 			
-			
 		} while(validacion==false);
+		
+		scan.close();
 	}
 		
 }
